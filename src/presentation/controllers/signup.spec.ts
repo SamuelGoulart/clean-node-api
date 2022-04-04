75e6c60bce6c7573008ede1e10979b4ea5d981fb
Samuel Goulart <62961331+SamuelGoulart@users.noreply.github.com>
2022-04-04 15:24:12 -0300

    test: ensure SignUpController returns 400 if no nmae is provided

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,17 @@
+import { SignUpController } from './signup'
+
+describe('Sign Controller', () => {
+    test('Should return 400 if no name is provided', () => {
+        const sut = new SignUpController()
+        const htttpRequest = {
+            body: {
+                name: 'any_name', 
+                email: 'any_email@mail.com',
+                password: 'any_password',
+                passwordConfirmation: 'any_password' 
+            }
+        }
+        const htttpResponse = sut.handle(htttpRequest)
+        expect(htttpResponse.statusCode).toBe(400)
+    })
+})
