8922038689d528cc47ce607ac4931c18c8dfe2ee
Samuel Goulart <62961331+SamuelGoulart@users.noreply.github.com>
2022-04-05 16:21:06 -0300

    test: ensure EmailValidatorAdapter returns false if validator returns false

--- jest.config.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**', '!<rootDir>/src/**/*-protocols.ts', '!**/protocols/**', '!**/test/**'],
-  coverageDirectory: "babel",
-  coverageProvider: "v8",
-  transform: {
-    '.+\\.ts$': 'ts-jest'
-  }
-};
