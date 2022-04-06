efa1c778104aa26860589b5986134cdb97fe3ea4
Samuel Goulart <62961331+SamuelGoulart@users.noreply.github.com>
2022-04-06 11:19:13 -0300

    feat: ensure BcryptAdapter calls bcrypt with correct value

---
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -0,0 +1,19 @@
+import bcrypt from 'bcrypt'
+import { Encrypter } from "../../data/protocols/encrypter"
+
+export class BcryptAdapter implements Encrypter {
+    private readonly salt: number
+
+    // Informações específicas da biblioteca são pelo constructor
+    // Nem todas as bibliotecas de criptografia tem o salt
+    constructor (salt: number){
+        this.salt = salt
+    }
+
+    // O método do encrypt, faz parte do protocolo, da interface do Encrypter
+    // No parâmetro do encrypt, só pode passar informções genéricas, que vão servir para qualquer biblioteca de criptografia
+    async encrypt(value: string): Promise<string> {
+        await bcrypt.hash(value, this.salt)
+        return null
+    }
+}
