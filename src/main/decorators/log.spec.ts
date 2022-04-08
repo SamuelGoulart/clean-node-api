ee2110d1374e1012f4f7acd9cb6e202b298478db
Samuel Goulart <62961331+SamuelGoulart@users.noreply.github.com>
2022-04-08 14:55:13 -0300

    test: ensure LogControllerDecorator calls LogErrorRepository if controller returns a server error

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -1,9 +1,16 @@
-import { LogControllerDecorator } from "./log"
-import { Controller, HttpRequest, HttpResponse } from "../../presentation/protocols"
+import { LogControllerDecorator } from './log'
+import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
+import { serverError } from '../../presentation/helpers/http-helper'
+import { LogErrorRepository } from '../../data/protocols/log-error-repository'
 
-interface SutTypes {
-    sut: LogControllerDecorator
-    controllerStub: Controller
+const makeLogErrorRepository = (): LogErrorRepository => {
+    class LogErrorRepository implements LogErrorRepository {
+        async log(stack: string): Promise<void> {
+            return new Promise(resolve => resolve())
+        }
+    }
+
+    return new LogErrorRepository()
 }
 
 const makeController = (): Controller => {
@@ -12,7 +19,7 @@ const makeController = (): Controller => {
             const httpResponse: HttpResponse = {
                 statusCode: 200,
                 body: {
-                    name: 'Rodrigo'
+                    name: 'Samuel'
                 }
             }
             return new Promise(resolve => resolve(httpResponse))
@@ -22,12 +29,21 @@ const makeController = (): Controller => {
     return new ControllerStub()
 }
 
+interface SutTypes {
+    sut: LogControllerDecorator
+    controllerStub: Controller,
+    logErrorRepositoryStub: LogErrorRepository
+}
+
 const makeSut = (): SutTypes => {
     const controllerStub = makeController()
-    const sut = new LogControllerDecorator(controllerStub)
+    const logErrorRepositoryStub = makeLogErrorRepository()
+    const sut = new LogControllerDecorator(controllerStub, logErrorRepositoryStub)
 
     return {
-        sut, controllerStub
+        sut, 
+        controllerStub, 
+        logErrorRepositoryStub
     }
 }
 
@@ -39,8 +55,8 @@ describe('LogController Decorato', () => {
             body: {
                 email: 'any_mail@mail.com',
                 name: '@any_name',
-                password: '',
-                passwordConfirmation: ''
+                password: 'any_password',
+                passwordConfirmation: 'any_password'
             }
         }
 
@@ -52,10 +68,10 @@ describe('LogController Decorato', () => {
         const { sut } = makeSut()
         const httpRequest = {
             body: {
-                email: 'any_mail@mail.com',
-                name: '@any_name',
-                password: '',
-                passwordConfirmation: ''
+                name: 'any_name',
+                email: 'any_email@mail.com',
+                password: 'any_password',
+                passwordConfirmation: 'any_password'
             }
         }
 
@@ -63,8 +79,28 @@ describe('LogController Decorato', () => {
         expect(httpResponse).toEqual({
             statusCode: 200,
             body: {
-                name: 'Rodrigo'
+                name: 'Samuel',
             }
         })
+    })
+
+    test('Should call LogErrorRepository with correct error if controller returns a server error', async () => {
+        const { sut, controllerStub, logErrorRepositoryStub } = makeSut()
+        const fakeError = new Error()
+        fakeError.stack = 'any_stack'
+        const error = serverError(fakeError)
+        const logSpy = jest.spyOn(logErrorRepositoryStub, 'log')
+        jest.spyOn(controllerStub, 'handle').mockReturnValueOnce(new Promise(resolve => resolve(error)))
+        const httpRequest = {
+            body: {
+                email: 'any_mail@mail.com',
+                name: '@any_name',
+                password: 'any_password',
+                passwordConfirmation: 'any_password'
+            }
+        }
+
+       await sut.handle(httpRequest)
+       expect(logSpy).toHaveBeenCalledWith('any_stack')
     })
 })
