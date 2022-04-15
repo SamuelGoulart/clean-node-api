49fa31bce5b14a23edee63967d449af303ff58e8
SamuelGoulart <samuel.a.goulart@gmail.com>
2022-04-15 14:20:02 -0300

    test: ensure DbAuthentication returns a token on success

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -37,7 +37,7 @@ const makeHashComparer = (): HashComparer => {
 
 const makeTokenGenerator = (): TokenGenerator => {
     class TokenGeneratorStub implements TokenGenerator {
-        async generate (id: string): Promise<String> {
+        async generate (id: string): Promise<string> {
             return new Promise(resolve => resolve('any_token'))
         }
     }
@@ -125,6 +125,12 @@ describe('DbAuthentication UseCase', () => {
         jest.spyOn(tokenGeneratorStub, 'generate').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error)))
         const promise = sut.auth(makeFakeAuthentication())
         await expect(promise).rejects.toThrow()
+    })
+
+    test('Should call TokenGenerator with correct id', async () => {
+        const { sut } = makeSut()
+        const accessToken = await sut.auth(makeFakeAuthentication())
+        expect(accessToken).toBe('any_token')
     })
 
 })
