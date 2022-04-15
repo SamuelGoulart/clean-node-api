0e8743a8e23fe8c9ab632a5f14318d29ed6eb3c6
SamuelGoulart <samuel.a.goulart@gmail.com>
2022-04-15 15:58:12 -0300

    feat: ensure DbAuthentication calls UpdateAccessTokenRepository with correct values

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -2,20 +2,25 @@ import { TokenGenerator } from './../../protocols/criptography/token-generator';
 import { HashComparer } from './../../protocols/criptography/hash-compare';
 import { AuthenticationModel, Authentication } from "../../../domain/usecases/authentication"
 import { LoadAccountByEmailRepository } from "../../protocols/db/load-account-by-email-repository";
+import { UpdateAcessTokenRepository } from '../../protocols/db/update-access-token-repository';
 
 export class DbAuthentication implements Authentication {
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
     private readonly hashComparer: HashComparer
     private readonly tokenGenerator: TokenGenerator
+    private readonly updateAcessTokenRepository: UpdateAcessTokenRepository
     
     constructor (
         loadAccountByEmailRepository: LoadAccountByEmailRepository,
         hashComparer: HashComparer,
-        tokenGenerator: TokenGenerator
+        tokenGenerator: TokenGenerator,
+        updateAcessTokenRepository: UpdateAcessTokenRepository
+
     ){
         this.loadAccountByEmailRepository = loadAccountByEmailRepository
         this.hashComparer = hashComparer
         this.tokenGenerator = tokenGenerator
+        this.updateAcessTokenRepository = updateAcessTokenRepository
     }
 
     async auth(authentication: AuthenticationModel): Promise<string> {
@@ -25,6 +30,7 @@ export class DbAuthentication implements Authentication {
             const isValid = await this.hashComparer.compare(authentication.password, account.password)
             if (isValid) {
                 const accessToken = await this.tokenGenerator.generate(account.id)
+                await this.updateAcessTokenRepository.update(account.id, accessToken)
                 return accessToken
             }
         }
