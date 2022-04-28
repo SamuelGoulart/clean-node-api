9f47c475bd17bc7b98f184b18fecd0cfc2a7de40
Samuel Goulart <62961331+SamuelGoulart@users.noreply.github.com>
2022-04-28 14:44:49 -0300

    refactor: rename signup routes with login routes

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest'
 import app from '../config/app'
 import  { MongoHelper } from '../../infra/db/mongodb-old/helpers/mongo-helper'
 
-describe('SignUp Routes', () => {
+describe('Login Routes', () => {
     
     beforeAll(async () => {
         await MongoHelper.connect(process.env.MONGO_URL)
@@ -17,16 +17,18 @@ describe('SignUp Routes', () => {
         await accountCollection.deleteMany({})
     })
 
-    test('Should return an account on success', async () => {
+    describe('POST /signup', () => {
+        test('Should return 200 on signup', async () => {
         
-        await request(app)
-            .post('/api/signup')
-            .send({
-                name: "Samuel Almeida",
-                email: "samuel.a.goulart@gmail.com",
-                password: "123456",
-                passwordConfirmation: "123456"
-            })
-            .expect(200)
+            await request(app)
+                .post('/api/signup')
+                .send({
+                    name: "Samuel Almeida",
+                    email: "samuel.a.goulart@gmail.com",
+                    password: "123456",
+                    passwordConfirmation: "123456"
+                })
+                .expect(200)
+        })
     })
 })
