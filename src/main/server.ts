1b47c7bc9638f934501e6fe12ca8c88b1b12565e
Samuel Goulart <samuel.a.goulart@gmail.com>
2022-04-29 08:56:56 -0300

    refactor: change routes to use filesystem instead of fast-glob

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,10 +1,6 @@
 // import mongoose from 'mongoose'
 import { MongoHelper } from '../infra/db/mongodb-old/helpers/mongo-helper'
-
-import app from './config/app';
-
 import env from './config/env'
-
 
 // (async ()=> {
 
@@ -22,7 +18,7 @@ import env from './config/env'
 
 MongoHelper.connect(env.mongoUrl).then(async () => {
     const app = (await import('./config/app')).default
-    app.listen(env.port, ()=> console.log(`Server runnig at ${env.port}`))
+    app.listen(env.port, () => console.log(`Server runnig at ${env.port}`))
 })
 .catch(console.error)
 
